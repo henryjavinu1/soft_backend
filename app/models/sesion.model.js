38a6579f3ea6c5ace701699d37050f5d5c5c1b70
Willian Josue <wjoe1995@live.com>
2022-06-26 15:17:04 -0600

    primera entrega

--- app/models/sesion.model.js
+++ app/models/sesion.model.js
@@ -14,6 +14,11 @@ module.exports = (sequelize, Sequelize) => {
         allowNull: false,
         defaultValue: false
       },
+      isActive: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
     });
   
     return Sesion;
