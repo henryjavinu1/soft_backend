1991897595bbfdb8890b9410417531ede72be2be
Astrid Hernandez <astridhndz@yahoo.com>
2022-06-28 17:43:17 -0600

    Modelos Producto y tipoProducto

---
+++ app/models/tipoproducto.model.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, Sequelize) => {
+    const TipoProducto = sequelize.define("tipoproducto", {
+      tipoProducto: {
+        type: Sequelize.STRING,
+        unique: true, //  no permite que se dupliquen nombres de tipos de productos
+        allowNull: false // no permite valores nulos
+      },
+      descripcionProducto: {
+        type: Sequelize.STRING,
+        allowNull: false // no permite valores nulos
+      },
+      isvTipoProducto: {
+        type: Sequelize.STRING,
+        allowNull: false, // no permite valores nulos
+      },
+      isDelete: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false, // no permite valores nulos
+        defaultValue: false // establece el valor por defecto en false
+      }
+    });
+    return TipoProducto;
+  };
+  
