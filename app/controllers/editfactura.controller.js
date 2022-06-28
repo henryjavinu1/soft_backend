e47d22dda8247a3737128e56b53518e459cecb57
Diego Garcia <diegofabriciogarcia@gmail.com>
2022-06-28 13:31:59 -0600

    Adición del controlador para editar

---
+++ app/controllers/editfactura.controller.js
@@ -0,0 +1,12 @@
+const { request, response } = require('express');
+//Aquí seguirían la importación de los modelos
+
+const CONTROLADORGet = async (req = request, res = response) => {}
+const CONTROLADORPost = async (req = request, res = response) => {}
+const CONTROLADORPut = async (req = request, res = response) => {}
+
+module.exports = {
+CONTROLADORGet,
+CONTROLADORPost,
+CONTROLADORPut,
+}
