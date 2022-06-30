55376a8b8e65beeb3d8c4454b59045cfa9c06c8b
henrryJavinu1 <henryjavinu1@gmail.com>
2022-06-29 19:37:36 -0600

    Update user.controller.js
    Creacion de funcion baja usuario

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,15 +1,38 @@
-exports.allAccess = (req, res) => {
-  res.status(200).send("Public Content.");
-};
+const db = require("../models/user");
+const config = require("../config/auth.config");
+const { user } = require("../models/user");
+const User = db.user;
+const Role = db.role;
+const Sesion = db.sesion;
+const Op = db.Sequelize.Op;
 
-exports.userBoard = (req, res) => {
-  res.status(200).send("User Content.");
-};
-
-exports.adminBoard = (req, res) => {
-  res.status(200).send("Admin Content.");
-};
-
-exports.moderatorBoard = (req, res) => {
-  res.status(200).send("Moderator Content.");
+exports.bajauser = async (req, res) => {
+  // BAJA a un usuario 
+  try {
+    const user = await user.findOne({
+      where: {
+        username: req.body.username,
+        IsDelete: false,
+      }
+    })
+    const userUpdate = await user.update({
+      IsDelete: true,
+    },{
+      where: 
+        username = req.body.username,
+    });
+    if (!bajauser){
+      return res.status(404).send({
+        message: "No se pudo dar de baja al usuario"
+      })
+    }else {
+      return res.send({
+        message: "Se le dio de baja exitosamente"
+      })
+    }
+  } catch (error) {
+    res.status(500).send({
+      message: error.message
+    });
+  }
 };
