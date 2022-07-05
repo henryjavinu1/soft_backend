7def74fca5b87a5526de63133d35ad2058ba78c9
Gustavo Rodriguez <gustavo_adolfo_rodriguez@outlook.com>
2022-07-05 01:42:54 -0600

    Commit de controladores y rutas

--- server.js
+++ server.js
@@ -41,7 +41,8 @@ require("./app/routes/user.routes")(app);
 require("./app/routes/generarfactura.routes")(app);
 require("./app/routes/manipularfactura.routes")(app);
 require("./app/routes/arqueo.routes")(app);
-// require("./app/routes/permisos.routes")(app);
+require("./app/routes/detalleventa.routes")(app);
+require("./app/routes/ventas.routes")(app);
 
 
 // set port, listen for requests
