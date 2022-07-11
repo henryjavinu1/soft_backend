ca26c19734e11d6c08179ed71a9f4779d1a61f97
Jhonatan Josue Osorto Centeno <jhonatanosorto39@gmail.com>
2022-07-10 19:28:12 -0600

    cambio de metodo en rutas

--- app/routes/empleado.routes.js
+++ app/routes/empleado.routes.js
@@ -10,8 +10,8 @@ module.exports = function(app) {
     });
       
     app.post("/api/empleado/crearempleado", controller.crearEmpleado);
-    app.get("/api/empleado/buscarempleado", controller.buscarEmpleado);
-    app.get("/api/empleado/buscarEmpleadoPorNombre", controller.buscarEmpleadoPorNombre);
+    app.post("/api/empleado/buscarempleado", controller.buscarEmpleado);
+    app.post("/api/empleado/buscarEmpleadoPorNombre", controller.buscarEmpleadoPorNombre);
     app.get("/api/empleado/traerTodosLosEmpleados", controller.traerTodosLosEmpleados); 
     app.put("/api/empleado/actualizarEmpleado", controller.actualizarEmpleado); 
     app.post("/api/empleado/eliminarEmpleado", controller.eliminarEmpleado);
