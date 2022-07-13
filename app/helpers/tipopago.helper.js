2c27fa306c979486e26b852b5fed359eb7e8ee95
KatuAgui <83032044+KatuAgui@users.noreply.github.com>
2022-07-12 17:04:16 -0700

    fghj
    fgh

---
+++ app/helpers/tipopago.helper.js
@@ -0,0 +1,17 @@
+
+const impresionDeTipoPago = (pagoBuscadas) => {
+    const tipopago = pagoBuscadas.map((tipopago) => {
+  
+        return {
+            idTipoPago: tipopago.idTipoPago,
+            tipoDePago: tipopago.tipoDePago,
+            descripcionTipoPago: tipopago.descripcionTipoPago,
+        }
+    });
+    return tipopago;
+}
+
+module.exports = {
+    impresionDeTipoPago,
+    // filtrarFacturas,
+}
