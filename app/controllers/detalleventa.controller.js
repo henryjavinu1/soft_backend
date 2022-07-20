54e0e3b9c9cd0954437f0a73dc9d694cb8c51250
Gustavo Rodriguez <gustavo_adolfo_rodriguez@outlook.com>
2022-07-19 20:10:58 -0600

    prueba

--- app/controllers/detalleventa.controller.js
+++ app/controllers/detalleventa.controller.js
@@ -52,3 +52,56 @@ exports.mostrarDetalles = async (req = request, res = response) => {
         })
     }
 }
+
+
+/*Actualizar 
+exports.actualizarCliente = async (req = request, res = response) => {
+    const dni = req.body.dni;
+    const { email, rtn, nombreCliente, direccion, telefonoCliente, isDelete } = req.body;
+    console.log(dni);
+    try {
+        const clienteBuscado = await Cliente.findOne({
+            where: {
+                id: req.body.id
+            },
+        });
+        if (!clienteBuscado) {
+            return res.status(404).send({
+                msg: "Cliente no existe"
+            })
+        }
+        validarCamposCliente(clienteBuscado, dni, email, rtn, nombreCliente, direccion, telefonoCliente, isDelete)
+        clienteBuscado.save();
+        return res.status(200).send({
+            clienteBuscado,
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            message: "Ocurrio un error" + error
+        })
+    }
+}
+//Eliminar
+exports.eliminarCliente = async (req, res) => {
+    try {
+        const eliminarCliente = await Cliente.update({
+            isDelete: true
+        }, {
+            where: {
+                id: req.body.id
+            }
+        });
+        if (eliminarCliente) {
+            res.status(200).send({
+                message: "Cliente eliminado correctamente"
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(401).send({
+            message: "Error al eliminar cliente: " + error.message
+        });
+    }
+}*/
