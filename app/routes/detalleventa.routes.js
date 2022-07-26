f2e6a8ae78a512926f704ea84d94027de4b2123b
Gustavo Rodriguez <gustavo_adolfo_rodriguez@outlook.com>
2022-07-25 18:19:39 -0600

    cambios pequeños

--- app/routes/detalleventa.routes.js
+++ app/routes/detalleventa.routes.js
@@ -14,5 +14,6 @@ module.exports = function(app) {
 
   app.post("/api/detalleventa", controller.creardetalleventa);
   app.post("/api/mostrardetalle", controller.mostrarDetalles);
+  app.post("/api/eliminardetalle", controller.eliminarDetalle);
   
 };
