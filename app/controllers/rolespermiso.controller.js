9dc17149bb22c261736c903af50352410ee2a14a
Jhonatan Josue Osorto Centeno <jhonatanosorto39@gmail.com>
2022-08-14 19:38:35 -0600

    realizando mantenimiento

--- app/controllers/rolespermiso.controller.js
+++ app/controllers/rolespermiso.controller.js
@@ -3,9 +3,11 @@ const { Op, DataTypes, Model } = require("sequelize");
 const db = require('../models/puntoDeVentas');
 const roles_pepermisos = db.roles_permisos;
 
-exports.crearolpermiso = async (req, res) => {
+exports.crearolpermiso = async (req = request, res) => {
+    const lista = req.body.lista;
+    console.log(req.body.lista);
     try {
-        const rol = await roles_pepermisos.bulkCreate(req.body);
+        const rol = await roles_pepermisos.bulkCreate(lista);
         return res.status(200).json({
             message: "rolpermiso creado con exito",
             data: rol,
